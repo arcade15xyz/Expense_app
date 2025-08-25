4a8743bc65ab4c9258503d1593044df0d6ca6637
arcade15xyz <arcade15xyz@gmail.com>
2025-08-25 10:05:27 +0530

    created dummy data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_app/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,6 +12,20 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: EnumCategory.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: EnumCategory.leisure,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,7 +2,7 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
-enum Category { food, travel, leisure, work }
+enum EnumCategory { food, travel, leisure, work }
 
 class Expense {
   Expense({
@@ -16,5 +16,5 @@ class Expense {
   final String title;
   final double amount;
   final DateTime date;
-  final Category category;
+  final EnumCategory category;
 }
