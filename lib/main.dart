9a6fc976655af73e263fdabb0639efd987488775
arcade15xyz <arcade15xyz@gmail.com>
2025-08-27 16:45:51 +0530

    setting text theme

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_app/widgets/expenses.dart';
+
+// from seed make different shades of the seed color applied .
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 152, 39, 216),
+);
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        colorScheme: kColorScheme,
+        // Theme for appbar
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.secondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: const Expenses(),
+    ),
+  );
+}
