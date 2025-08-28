9b458fc59dd56e36478c8adeca133850193c876f
arcade15xyz <arcade15xyz@gmail.com>
2025-08-28 11:58:46 +0530

    Using `ExpenseBucket.forCategory` for getting each category's sum amount. and inserting chart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -35,9 +35,14 @@ class Expense {
 
 class ExpenseBucket {
   const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+          .where((expense) => expense.category == category)
+          .toList();
+
   final EnumCategory category;
   final List<Expense> expenses;
-
   double get totalExpenses {
     double sum = 0;
     for (final expense in expenses) {
